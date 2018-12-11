57c1647ad9636035f792121859db8168638772a4
bal-lon <Foxmail-qtum@foxmail.com>
2018-12-11 18:05:31 +0800

    增加 log、rest 返回信息常量类

--- SCE-Core/src/main/java/cn/com/bonc/sce/constants/MessageConstants.java
+++ SCE-Core/src/main/java/cn/com/bonc/sce/constants/MessageConstants.java
@@ -6,5 +6,30 @@ package cn.com.bonc.sce.constants;
  * @since 2018/12/11 16:56
  */
 public interface MessageConstants {
-    String SCE_MSG_000 = "系统启动 {}";
+    /**
+     * 1. 系统初始化相关
+     * 0 - 199
+     */
+    String SCE_MSG_0000 = "服务 {} 启动, ip: {}, port: []";
+
+    /**
+     * 2. 文件操作相关
+     * 200 - 399
+     */
+    String SCE_MSG_0200 = "读取文件： {}";
+
+    /**
+     * 3. 数据库操作相关
+     * 400 - 599
+     */
+
+    /**
+     * 4. 网络异常
+     * 600 - 799
+     */
+
+    /**
+     * Spring 框架自身异常
+     * 800 - 999
+     */
 }
---
+++ SCE-Core/src/main/java/cn/com/bonc/sce/constants/PortalMessageConstants.java
@@ -0,0 +1,10 @@
+package cn.com.bonc.sce.constants;
+
+/**
+ * @author Leucippus
+ * @version 0.1
+ * @since 2018/12/11 17:35
+ */
+public interface PortalMessageConstants {
+    String SCE_PORTAL_MSG_000 = MessageConstants.SCE_MSG_0000;
+}
