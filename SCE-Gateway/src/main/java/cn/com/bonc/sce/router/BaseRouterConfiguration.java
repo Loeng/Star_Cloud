636c2a475ce395db027b328a462aaa02bb822bec
bal-lon <Foxmail-qtum@foxmail.com>
2018-12-19 10:07:42 +0800

    将 gateway 从 zuul 切换到了 spring cloud gateway，并添加了基础 router 组件。

---
+++ SCE-Gateway/src/main/java/cn/com/bonc/sce/router/BaseRouterConfiguration.java
@@ -0,0 +1,29 @@
+package cn.com.bonc.sce.router;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Leucippus
+ * @version 0.1
+ * @since 2018/12/18 16:08
+ */
+@Slf4j
+@Controller
+@SpringBootConfiguration
+public class BaseRouterConfiguration {
+
+    @Bean
+    public RouteLocator myRoutes( RouteLocatorBuilder builder ) {
+        return builder.routes()
+                .route( config -> config
+                        .path( "/api-management$" )
+                        .filters( f -> f.addRequestHeader( "Hello", "World" ) )
+                        .uri( "http://httpbin.org:80" ) )
+                .build();
+    }
+}
