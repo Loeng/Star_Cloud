01870627449610446607ae00e19a7cc1fbe1b130
yuehaibo <yuehaibo@bonc.com.cn>
2018-12-21 10:56:09 +0800

    这次是厂商信息。

--- SCE-DataAccess/src/main/java/cn/com/bonc/sce/api/CompanyInfoApiController.java
+++ SCE-DataAccess/src/main/java/cn/com/bonc/sce/api/CompanyInfoApiController.java
@@ -55,10 +55,10 @@ public class CompanyInfoApiController {
         map2.put( "companyStatus", 1 );
         resultList.add( map1 );
         resultList.add( map2 );
-        Map< String, Object > resultMap = new HashMap<>(  );
-        resultMap.put( "data" ,resultList);
-        resultMap.put( "totalCount" ,32);
-        resultMap.put( "totalPage",4 );
+        Map< String, Object > resultMap = new HashMap<>();
+        resultMap.put( "data", resultList );
+        resultMap.put( "totalCount", 32 );
+        resultMap.put( "totalPage", 4 );
         restRecord.setData( resultMap );
         return restRecord;
     }
@@ -74,8 +74,11 @@ public class CompanyInfoApiController {
     @ResponseBody
     public RestRecord addCompanyInfo(
             @RequestBody CompanyInfo companyInfo ) {
-        RestRecord restRecord = new RestRecord(  );
-        restRecord.setMsg( PortalMessageConstants.SCE_PORTAL_MSG_200 );
+        RestRecord restRecord = new RestRecord();
+        Map< String, Object > resultMap = new HashMap<>();
+        restRecord.setMsg( "This is ApiController response message ! Method name addCompanyInfo" );
+        resultMap.put( "Param1", companyInfo );
+        restRecord.setData( resultMap );
         return restRecord;
 //        return new RestRecord( 0, companyInfoRepository.addCompanyInfo( companyInfo ) );
     }
@@ -87,13 +90,17 @@ public class CompanyInfoApiController {
      * @param companyInfo 用户输入的厂商信息
      * @return 返回是否更新成功
      */
-    @PatchMapping( "/{companyId}" )
+    @PostMapping( "/{companyId}" )
     @ResponseBody
     public RestRecord updateCompanyInfo(
             @PathVariable( "companyId" ) String companyId,
             @RequestBody CompanyInfo companyInfo ) {
-        RestRecord restRecord = new RestRecord(  );
-        restRecord.setMsg( PortalMessageConstants.SCE_PORTAL_MSG_200 );
+        RestRecord restRecord = new RestRecord();
+        Map< String, Object > resultMap = new HashMap<>();
+        restRecord.setMsg( "This is ApiController response message ! Method name updateCompanyInfo" );
+        resultMap.put( "Param1", companyId );
+        resultMap.put( "Param2", companyInfo );
+        restRecord.setData( resultMap );
         return restRecord;
 //        return new RestRecord( 0, companyInfoRepository.updateCompanyInfo( companyId, companyInfo ) );
     }
@@ -109,8 +116,11 @@ public class CompanyInfoApiController {
     @ResponseBody
     public RestRecord deleteCompanyInfo(
             @PathVariable( "companyId" ) String companyId ) {
-        RestRecord restRecord = new RestRecord(  );
-        restRecord.setMsg( PortalMessageConstants.SCE_PORTAL_MSG_200 );
+        RestRecord restRecord = new RestRecord();
+        Map< String, Object > resultMap = new HashMap<>();
+        restRecord.setMsg( "This is ApiController response message ! Method name deleteCompanyInfo" );
+        resultMap.put( "Param1", companyId );
+        restRecord.setData( resultMap );
         return restRecord;
 //        return new RestRecord( 0, companyInfoRepository.deleteCompanyInfo( companyId ) );
     }
