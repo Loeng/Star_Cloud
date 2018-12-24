57dfcd2375d5d675fbb667858f43768ee5817c7c
tanglizhong <tanglizhonrg@bonc.com.cn>
2018-12-24 22:32:59 +0800

    跨域

--- SCE-AuthenticationServer/src/main/java/cn/com/bonc/sce/AuthenticationServiceApplication.java
+++ SCE-AuthenticationServer/src/main/java/cn/com/bonc/sce/AuthenticationServiceApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -23,4 +26,17 @@ public class AuthenticationServiceApplication {
         SpringApplication.run( AuthenticationServiceApplication.class, args );
     }
 
+    /**
+     * Rest 跨域配置
+     */
+    @Bean
+    public WebMvcConfigurer restConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings( CorsRegistry registry ) {
+                registry.addMapping( "/**/*" ).allowedOrigins( "*" ).allowedMethods( "*" ).allowCredentials( true );
+            }
+
+        };
+    }
 }
