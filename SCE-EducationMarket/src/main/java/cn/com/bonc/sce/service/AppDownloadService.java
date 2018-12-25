0c0cb0867c3eec973c4954efb6346f396099cf2a
tanglizhong <tanglizhonrg@bonc.com.cn>
2018-12-25 16:54:17 +0800

    修改

--- SCE-EducationMarket/src/main/java/cn/com/bonc/sce/service/AppDownloadService.java
+++ SCE-EducationMarket/src/main/java/cn/com/bonc/sce/service/AppDownloadService.java
@@ -4,6 +4,7 @@ import cn.com.bonc.sce.dao.AppDownloadDao;
 import cn.com.bonc.sce.dao.AppManageDao;
 import cn.com.bonc.sce.rest.RestRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @Date: 2018/12/25 15:44
  * @Description:
  */
+@Service
 public class AppDownloadService {
 
     private AppDownloadDao appDownloadDao;
