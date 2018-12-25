5562a02c6a8a6ffab4a29c0b254688c6eee1c8f4
bal-lon <Foxmail-qtum@foxmail.com>
2018-12-25 20:06:02 +0800

    添加 gateway - CORS 配置。

--- SCE-Gateway/src/main/java/cn/com/bonc/sce/config/CorsConfiguration.java
+++ SCE-Gateway/src/main/java/cn/com/bonc/sce/config/CorsConfiguration.java
@@ -28,21 +28,21 @@ public class CorsConfiguration {
 
     @Bean
     public WebFilter corsFilter() {
-        return ( ServerWebExchange ctx, WebFilterChain chain) -> {
+        return ( ServerWebExchange ctx, WebFilterChain chain ) -> {
             ServerHttpRequest request = ctx.getRequest();
-            if (CorsUtils.isCorsRequest(request)) {
+            if ( CorsUtils.isCorsRequest( request ) ) {
                 ServerHttpResponse response = ctx.getResponse();
                 HttpHeaders headers = response.getHeaders();
-                headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
-                headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
-                headers.add("Access-Control-Max-Age", MAX_AGE);
-                headers.add("Access-Control-Allow-Headers",ALLOWED_HEADERS);
-                if (request.getMethod() == HttpMethod.OPTIONS) {
-                    response.setStatusCode(HttpStatus.OK);
+                headers.add( "Access-Control-Allow-Origin", ALLOWED_ORIGIN );
+                headers.add( "Access-Control-Allow-Methods", ALLOWED_METHODS );
+                headers.add( "Access-Control-Max-Age", MAX_AGE );
+                headers.add( "Access-Control-Allow-Headers", ALLOWED_HEADERS );
+                if ( request.getMethod() == HttpMethod.OPTIONS ) {
+                    response.setStatusCode( HttpStatus.OK );
                     return Mono.empty();
                 }
             }
-            return chain.filter(ctx);
+            return chain.filter( ctx );
         };
     }
 
