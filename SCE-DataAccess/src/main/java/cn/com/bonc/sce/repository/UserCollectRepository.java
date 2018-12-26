01e08bc5ee805acfe4f5d9929cbf6e849e1a03f2
yinming <yinming@bonc.com.cn>
2018-12-26 21:25:33 +0800

    收藏app接口修改sql

--- SCE-DataAccess/src/main/java/cn/com/bonc/sce/repository/UserCollectRepository.java
+++ SCE-DataAccess/src/main/java/cn/com/bonc/sce/repository/UserCollectRepository.java
@@ -15,7 +15,7 @@ public interface UserCollectRepository extends JpaRepository<App,String> {
 
 
     @Modifying
-    @Query( value = "select T2.APP_NAME APP_NAME,T2.COMPANY_ID COMPANY_ID,T2.APP_NOTES APP_NOTES,T2.APP_PHONE_PIC APP_PHONE_PIC " +
+    @Query( value = "select T2.APP_NAME APP_NAME,T1.APP_ID APP_ID,T2.APP_NOTES APP_NOTES,T2.APP_LINK APP_LINK,null APP_ICON " +
             "from STARCLOUDMARKET.SCE_USER_APP_COLLECTION T1 left join STARCLOUDMARKET.SCE_MARKET_APP_INFO T2" +
             " on T1.APP_ID = T2.APP_ID " +
             "WHERE T1.IS_DELETE = 1 and T1.USER_ID = :userId   ", nativeQuery = true )
