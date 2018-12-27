4f99558bec6d107dbea517d0cf668d371557db6b
moukunlin@bonc.com.cn <moukunlin@bonc.com.cn>
2018-12-27 20:54:40 +0800

    学生用户信息查询增加 是否允许登录查询

--- SCE-DataAccess/src/main/java/cn/com/bonc/sce/dao/UserInfoRepository.java
+++ SCE-DataAccess/src/main/java/cn/com/bonc/sce/dao/UserInfoRepository.java
@@ -29,9 +29,8 @@ public interface UserInfoRepository extends JpaRepository<FamilyInfoEntity,Long>
     Page<Map<String,Object>> findTeacherByCondition(String name, String account, String organization_name, Pageable pageable);
 
      // 查询和搜索 学校 -> 学生 的列表信息
-    @Query(value = "SELECT * FROM STARCLOUDPORTAL.v_student_info WHERE USER_NAME LIKE CONCAT('%',CONCAT(?1,'%'))  AND USER_ACCOUNT LIKE CONCAT('%',CONCAT(?2,'%'))  AND  AUTHORITY_NAME LIKE CONCAT('%',CONCAT(?3,'%'))",nativeQuery = true)
-    Page<Map<String,Object>> findSchoolByCondition(String name, String account, String organization_name, Pageable pageable);
-
+    @Query(value = "SELECT * FROM STARCLOUDPORTAL.v_student_info WHERE USER_NAME LIKE CONCAT('%',CONCAT(?1,'%'))  AND USER_ACCOUNT LIKE CONCAT('%',CONCAT(?2,'%'))  AND  AUTHORITY_NAME LIKE CONCAT('%',CONCAT(?3,'%')) AND LOGIN_PERMISSION_STATUS LIKE CONCAT('%',CONCAT(?4,'%'))",nativeQuery = true)
+    Page<Map<String,Object>> findSchoolByCondition(String name, String account, String organization_name,String login, Pageable pageable);
 
     // 查询和搜索 学校 -> 家长 的列表信息 // countQuery = "SELECT COUNT(*) FROM \"v_family_info\"  "
    @Query( value = "SELECT * FROM STARCLOUDPORTAL.v_family_info WHERE USER_NAME LIKE CONCAT('%',CONCAT(?1,'%')) AND USER_ACCOUNT LIKE CONCAT('%',CONCAT(?2,'%'))", nativeQuery = true)
