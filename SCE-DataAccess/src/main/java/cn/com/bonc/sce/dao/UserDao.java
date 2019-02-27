b1ac27edeb5e8a6260b135de54b493fae2895ad9
tanglizhong <tanglizhonrg@bonc.com.cn>
2019-02-27 16:59:26 +0800

    更新登陆时间

--- SCE-DataAccess/src/main/java/cn/com/bonc/sce/dao/UserDao.java
+++ SCE-DataAccess/src/main/java/cn/com/bonc/sce/dao/UserDao.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
-/**
+/**1
  * @author Leucippus
  * @version 0.1
  * @since 2018/12/11 7:50
@@ -22,7 +22,7 @@ public interface UserDao extends JpaRepository< User, String > {
     // TODO 必须考虑并发修改的问题
     @Transactional
     @Modifying
-    @Query( "UPDATE User user SET user.isFirstLogin = :isFirstLogin,user.lastLoginTimelastLoginTimelastLoginTime = sysdate WHERE user.userId = :userId" )
+    @Query( "UPDATE User user SET user.isFirstLogin = :isFirstLogin,user.lastLoginTime = sysdate WHERE user.userId = :userId" )
     public int updateUserLoginStatus( @Param( "userId" ) String userId, @Param( "isFirstLogin" ) Integer isFirstLogin );
 
     public User findUserByLoginName( String loginName );
