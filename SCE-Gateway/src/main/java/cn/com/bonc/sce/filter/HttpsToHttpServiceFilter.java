5c62d2791c1bbce67611076fa10b7f7e40fdc3f5
bal-lon <Foxmail-qtum@foxmail.com>
2019-03-20 17:11:20 +0800

    添加 HTTPS-SSL 设置，添加 HTTPS 转换为 HTTP 的过滤器

---
+++ SCE-Gateway/src/main/java/cn/com/bonc/sce/filter/HttpsToHttpServiceFilter.java
@@ -0,0 +1,54 @@
+package cn.com.bonc.sce.filter;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
+
+/**
+ * 当 Gateway 开启了 HTTPS 访问控制以后，Gateway 接收到发来的 HTTPS 请求后其转发给后台的请求也会同样使用 HTTPS，如果项目
+ * 中的微服务没有启用 SSL，则请求就会报错中断，故此 filter 会在 Gateway 转发 HTTPS 请求给后台微服务之前将请求转化为 HTTP
+ * 请求。
+ * @author JoeTH
+ * @version 1.1
+ * @since 2019/3/20 16:39
+ */
+@ConditionalOnProperty(name = "sce.server.https.true")
+@Component
+public class HttpsToHttpServiceFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono< Void > filter( ServerWebExchange exchange, GatewayFilterChain chain ) {
+        Object uriObj = exchange.getAttributes().get( GATEWAY_REQUEST_URL_ATTR );
+        if ( uriObj != null ) {
+            URI uri = ( URI ) uriObj;
+            uri = this.upgradeConnection( uri, "http" );
+            exchange.getAttributes().put( GATEWAY_REQUEST_URL_ATTR, uri );
+        }
+        return chain.filter( exchange );
+    }
+
+    private URI upgradeConnection( URI uri, String scheme ) {
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromUri( uri ).scheme( scheme );
+        if ( uri.getRawQuery() != null ) {
+            // When building the URI, UriComponentsBuilder verify the allowed characters and does not
+            // support the '+' so we replace it for its equivalent '%20'.
+            // See issue https://jira.spring.io/browse/SPR-10172
+            uriComponentsBuilder.replaceQuery( uri.getRawQuery().replace( "+", "%20" ) );
+        }
+        return uriComponentsBuilder.build( true ).toUri();
+    }
+
+    @Override
+    public int getOrder() {
+        return 10101;
+    }
+}
